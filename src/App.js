ebcb66154065fa210a2df8545ef75c0420866350
Norbert Csipak <norbertcipak@me.com>
2022-11-10 20:03:15 +0000

    added a horizontal line under the navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="justify-between mt-3 mr-3">
+    <div className="justify-between mr-3">
+      <hr class="my-8 h-px w-90 ml-3 bg-gray-200 border-0 dark:bg-gray-700"/>
       <Navbar />
       <hr class="my-8 h-px w-90 ml-3 bg-gray-200 border-0 dark:bg-gray-700"/>
     </div>
