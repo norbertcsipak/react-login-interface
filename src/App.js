42fb60a3b3eedb2cbb0c56b67d85a58e01b8df38
Norbert Csipak <norbertcipak@me.com>
2022-11-10 20:01:51 +0000

    added a horizontal line under the navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="flex justify-between mt-3 mr-3">
+    <div className="justify-between mt-3 mr-3">
       <Navbar />
+      <hr class="my-8 h-px w-90 ml-3 bg-gray-200 border-0 dark:bg-gray-700"/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
     <div
-      className="container flex justify-around mt-3 h-50">
-        <img src="../norbertcsipak-logo.jpeg" width="100px" height="100px" alt="norbertcsipak-logo"/>
+      className="container flex justify-around mt-3">
+        <img src="../norbertcsipak-logo.jpeg" width="100px" alt="norbertcsipak-logo"/>
         <a href="#" className="pt-6">Home</a>
         <a href="#" className="pt-6">About me</a>
         <a href="#" className="pt-6">Contact me</a>
