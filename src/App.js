bf64c96668989a807246b82858be0f0c4b025ccd
Norbert Csipak <norbertcipak@me.com>
2022-11-10 19:54:18 +0000

    navbar component for my portfolio page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Button from './components/Button';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="flex justify-between mt-3 mr-3">
       <Navbar />
-      <Button />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 const Navbar = () => {
   return (
-    <a href="#">
-      Home
-    </a>
+    <div
+      className="container flex justify-around mt-3 h-50">
+        <img src="../norbertcsipak-logo.jpeg" width="100px" height="100px" alt="norbertcsipak-logo"/>
+        <a href="#" className="pt-6">Home</a>
+        <a href="#" className="pt-6">About me</a>
+        <a href="#" className="pt-6">Contact me</a>
+        <a href="#" className="pt-6">Log in</a>
+    </div>
   )
 }
 
