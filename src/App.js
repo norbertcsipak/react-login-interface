2fbc38b79598c7003dc162dbdd7b268253fca8a3
Norbert Csipak <norbertcipak@me.com>
2022-11-10 20:18:33 +0000

    banner image added to the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="justify-between mr-3">
       <hr class="my-8 h-px w-90 ml-3 bg-gray-200 border-0 dark:bg-gray-700"/>
       <Navbar />
-      <hr class="my-8 h-px w-90 ml-3 bg-gray-200 border-0 dark:bg-gray-700"/>
+      <hr class="my-3 h-px w-90 ml-3 bg-gray-200 border-0 dark:bg-gray-700"/>
+      <Banner />
     </div>
   );
 }
